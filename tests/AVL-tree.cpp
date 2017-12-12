4bcb4b49f5a36bbf927355b524337ca4e06356e9
kate-lozovaya <katekalozovaya@gmail.com>
2017-12-12 21:47:54 +0300

    Update AVL-tree.cpp

--- tests/AVL-tree.cpp
+++ tests/AVL-tree.cpp
@@ -7,12 +7,12 @@ TEST_CASE("insert1", "[root]")
 {
    AVL_tree<int> tree;
    tree.insert(10);
-   REQUIRE(tree.getLeft(10) == nullptr);
-   REQUIRE(tree.getRight(10) == nullptr);
-   REQUIRE(tree.getParent(10) == nullptr);
-   REQUIRE(tree.getRoot() == tree.search(10));
-   REQUIRE(tree.height_(tree.search(10)) == 1);
-   REQUIRE(tree.count_() == 1);
+   REQUIRE(tree.left(10) == nullptr);
+   REQUIRE(tree.right(10) == nullptr);
+   REQUIRE(tree.parent(10) == nullptr);
+   REQUIRE(tree.root() == tree.search(10));
+   REQUIRE(tree.height(tree.search(10)) == 1);
+   REQUIRE(tree.count() == 1);
 }
 
 /*        10             9
@@ -27,16 +27,16 @@ TEST_CASE("insert2", "[rotate_right]")
    tree.insert(9);
    tree.insert(8);
    Node<int> * node = tree.search(9);
-   REQUIRE(tree.getLeft(9) == tree.search(8));
-   REQUIRE(tree.getRight(9) == tree.search(10));
-   REQUIRE(tree.getParent(9) == nullptr);
-   REQUIRE(tree.getRoot() == node);
-   REQUIRE(tree.height_(node) == 2);
-   REQUIRE(tree.getParent(8) == node);
-   REQUIRE(tree.getParent(10) == node);
-   REQUIRE(tree.height_(tree.search(8)) == 1);
-   REQUIRE(tree.height_(tree.search(10)) == 1);
-   REQUIRE(tree.count_() == 3);
+   REQUIRE(tree.left(9) == tree.search(8));
+   REQUIRE(tree.right(9) == tree.search(10));
+   REQUIRE(tree.parent(9) == nullptr);
+   REQUIRE(tree.root() == node);
+   REQUIRE(tree.height(node) == 2);
+   REQUIRE(tree.parent(8) == node);
+   REQUIRE(tree.parent(10) == node);
+   REQUIRE(tree.height(tree.search(8)) == 1);
+   REQUIRE(tree.height(tree.search(10)) == 1);
+   REQUIRE(tree.count() == 3);
 }
 
 /*  8                        9
@@ -50,35 +50,35 @@ TEST_CASE("insert3", "[rotate_left]")
    tree.insert(8);
    tree.insert(9);
    tree.insert(10);
-   REQUIRE(tree.getLeft(9) == tree.search(8));
-   REQUIRE(tree.getRight(9) == tree.search(10));
-   REQUIRE(tree.getParent(9) == nullptr);
-   REQUIRE(tree.getRoot() == tree.search(9));
-   REQUIRE(tree.height_(tree.search(9)) == 2);
-   REQUIRE(tree.getParent(8) == tree.search(9));
-   REQUIRE(tree.getParent(10) == tree.search(9));
-   REQUIRE(tree.height_(tree.search(8)) == 1);
-   REQUIRE(tree.height_(tree.search(10)) == 1);
-   REQUIRE(tree.count_() == 3);
+   REQUIRE(tree.left(9) == tree.search(8));
+   REQUIRE(tree.right(9) == tree.search(10));
+   REQUIRE(tree.parent(9) == nullptr);
+   REQUIRE(tree.root() == tree.search(9));
+   REQUIRE(tree.height(tree.search(9)) == 2);
+   REQUIRE(tree.parent(8) == tree.search(9));
+   REQUIRE(tree.parent(10) == tree.search(9));
+   REQUIRE(tree.height(tree.search(8)) == 1);
+   REQUIRE(tree.height(tree.search(10)) == 1);
+   REQUIRE(tree.count() == 3);
 }
 
 /*        9                9
         /   \     ->     /
        8     10         8        */
-TEST_CASE("remove1", "[leaf]") 
+TEST_CASE("delete1", "[leaf]") 
 {
    AVL_tree<int> tree;
    tree.insert(8);
    tree.insert(9);
    tree.insert(10);
-   REQUIRE(tree.count_() == 3);
-   tree.remove(10);
-   REQUIRE(tree.getRoot() == tree.search(9));
-   REQUIRE(tree.height_(tree.search(8)) == 1);
-   REQUIRE(tree.height_(tree.search(9)) == 2);
-   REQUIRE(tree.getRight(9) == nullptr);
+   REQUIRE(tree.count() == 3);
+   tree.deleteElement(10);
+   REQUIRE(tree.root() == tree.search(9));
+   REQUIRE(tree.height(tree.search(8)) == 1);
+   REQUIRE(tree.height(tree.search(9)) == 2);
+   REQUIRE(tree.right(9) == nullptr);
    REQUIRE(tree.search(10) == nullptr);
-   REQUIRE(tree.count_() == 2);
+   REQUIRE(tree.count() == 2);
 }
 
 /*        9                9
@@ -86,45 +86,45 @@ TEST_CASE("remove1", "[leaf]")
        8     10         7     10    
       /                          
      7                            */
-TEST_CASE("remove2", "[node->left, !node->right]") 
+TEST_CASE("delete2", "[node->left, !node->right]") 
 {
    AVL_tree<int> tree;
    tree.insert(8);
    tree.insert(9);
    tree.insert(10);
    tree.insert(7);
-   REQUIRE(tree.height_(tree.search(7)) == 1);
-   REQUIRE(tree.height_(tree.search(10)) == 1);
-   REQUIRE(tree.height_(tree.search(8)) == 2);
-   REQUIRE(tree.height_(tree.search(9)) == 3);
-   REQUIRE(tree.count_() == 4);
-   tree.remove(8);
-   REQUIRE(tree.getRoot() == tree.search(9));
-   REQUIRE(tree.height_(tree.search(7)) == 1);
-   REQUIRE(tree.height_(tree.search(10)) == 1);
-   REQUIRE(tree.height_(tree.search(9)) == 2);
-   REQUIRE(tree.getLeft(9) == tree.search(7));
+   REQUIRE(tree.height(tree.search(7)) == 1);
+   REQUIRE(tree.height(tree.search(10)) == 1);
+   REQUIRE(tree.height(tree.search(8)) == 2);
+   REQUIRE(tree.height(tree.search(9)) == 3);
+   REQUIRE(tree.count() == 4);
+   tree.deleteElement(8);
+   REQUIRE(tree.root() == tree.search(9));
+   REQUIRE(tree.height(tree.search(7)) == 1);
+   REQUIRE(tree.height(tree.search(10)) == 1);
+   REQUIRE(tree.height(tree.search(9)) == 2);
+   REQUIRE(tree.left(9) == tree.search(7));
    REQUIRE(tree.search(8) == nullptr);
-   REQUIRE(tree.count_() == 3);
+   REQUIRE(tree.count() == 3);
 }
 
 /*        9                10
         /   \     ->     /
        8     10         8        */
-TEST_CASE("remove3", "[node->right]") 
+TEST_CASE("delete3", "[node->right]") 
 {
    AVL_tree<int> tree;
    tree.insert(8);
    tree.insert(9);
    tree.insert(10);
-   REQUIRE(tree.count_() == 3);
-   tree.remove(9);
-   REQUIRE(tree.getRoot() == tree.search(10));
-   REQUIRE(tree.height_(tree.search(8)) == 1);
-   REQUIRE(tree.height_(tree.search(10)) == 2);
-   REQUIRE(tree.getLeft(10) == tree.search(8));
-   REQUIRE(tree.getRight(10) == nullptr);
-   REQUIRE(tree.getParent(10) == nullptr);
+   REQUIRE(tree.count() == 3);
+   tree.deleteElement(9);
+   REQUIRE(tree.root() == tree.search(10));
+   REQUIRE(tree.height(tree.search(8)) == 1);
+   REQUIRE(tree.height(tree.search(10)) == 2);
+   REQUIRE(tree.left(10) == tree.search(8));
+   REQUIRE(tree.right(10) == nullptr);
+   REQUIRE(tree.parent(10) == nullptr);
    REQUIRE(tree.search(9) == nullptr);
-   REQUIRE(tree.count_() == 2);
+   REQUIRE(tree.count() == 2);
 }
