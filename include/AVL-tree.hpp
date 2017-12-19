fa17c97ad1eedb07dd482ea89ba6820dc6f7c465
BinaryTreesImplementation <33779350+BinaryTreesImplementation@users.noreply.github.com>
2017-12-19 08:37:24 +0300

    Update AVL-tree.hpp

--- include/AVL-tree.hpp
+++ include/AVL-tree.hpp
@@ -1,16 +1,5 @@
 #include<iostream>
-
-#ifndef Counter
-class Counter
-{
-protected:
-	size_t& Count() { static size_t counter = 0; return counter; }
-
-public:
-	Counter() { ++Count(); }
-	~Counter() { --Count(); }
-};
-#endif
+#include "Counter.hpp"
 
 template<typename T>
 class AVL_tree
